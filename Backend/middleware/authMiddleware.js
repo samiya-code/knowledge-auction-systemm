024a14409cad780f5055ff0a7f21418e4651a7ec
tinsubb <tinsaebirhanub@gmail.com>
2026-04-03 07:28:52 -0700

    commited

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -7,7 +7,7 @@ const authenticateToken = async (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
 
-    if (!token) {
+    cdif (!token) {
       return res.status(401).json({
         success: false,
         message: 'Access token required'
