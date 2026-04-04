9a84ef37c01ea02b048deafc45b255955feaa2f9
zion-tsi <tsimelese99@gmail.com>
2026-04-04 01:42:16 -0900

    commited

--- Frontend/src/pages/CourseDetails.jsx
+++ Frontend/src/pages/CourseDetails.jsx
@@ -436,7 +436,7 @@ const CourseDetails = () => {
                   </div>
                 </div>
               </div>
-              <p className="text-sm text-gray-600">{course.instructor.bio}</p>
+              <p className="text-sm text-gray-700">{course.instructor.bio}</p>
             </div>
           </div>
         </div>
